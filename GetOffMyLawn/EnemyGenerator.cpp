f2c4de517051ffa65c696c184d1cc705af8e73f4
JamesPreston1993 <jamescpreston93@gmail.com>
2015-03-07 20:03:15 +0000

    Changes made to support new class name Configuration and probability of enemies spawning - 07/03/2015

--- GetOffMyLawn/EnemyGenerator.cpp
+++ GetOffMyLawn/EnemyGenerator.cpp
@@ -3,14 +3,14 @@
 EnemyGenerator::EnemyGenerator()
 {
 	this->generateTime = 0;
-	this->neighbourChance = 50;
-	this->salesmanChance = 30;
+	this->neighbourChance = 65;
+	this->salesmanChance = 25;
 	this->dogChance = 20;
 }
 
 GenericEnemy* EnemyGenerator::generateEnemy()
 {
-	if (this->generateTime == 150)
+	if (this->generateTime == 100)
 	{
 		// Random enemy at random time		
 		this->generateTime = 0;
@@ -18,14 +18,14 @@ GenericEnemy* EnemyGenerator::generateEnemy()
 		srand(time(NULL));
 
 		unsigned short random = rand() % 100;
-		unsigned short xPos = rand() % (ConfigurationManager::getScreenWidth() / ConfigurationManager::getSpriteWidth());
+		unsigned short xPos = rand() % (Configuration::getScreenWidth() / Configuration::getSpriteWidth());
 
 		if (random < neighbourChance)
-			return new Neighbour(xPos * ConfigurationManager::getSpriteWidth(), -ConfigurationManager::getSpriteHeight());
+			return new Neighbour(xPos * Configuration::getSpriteWidth(), -Configuration::getSpriteHeight());
 		else if (random < neighbourChance + salesmanChance)
-			return new Salesman(xPos * ConfigurationManager::getSpriteWidth(), -ConfigurationManager::getSpriteHeight());
+			return new Salesman(xPos * Configuration::getSpriteWidth(), -Configuration::getSpriteHeight());
 		else
-			return new Dog(xPos * ConfigurationManager::getSpriteWidth(), -ConfigurationManager::getSpriteHeight());
+			return new Dog(xPos * Configuration::getSpriteWidth(), -Configuration::getSpriteHeight());
 	}
 	else
 	{
