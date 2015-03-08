3275903b76f64551b7fd2e4743562125861bce3b
JamesPreston1993 <jamescpreston93@gmail.com>
2015-03-08 18:15:23 +0000

    Dog now moves in a zig-zag pattern - 08/03/2015

--- GetOffMyLawn/Dog.cpp
+++ GetOffMyLawn/Dog.cpp
@@ -5,15 +5,38 @@ Dog::Dog(short x, short y) : GenericEnemy(x, y)
 	this->setSpriteID(4);
 	this->setSpeed(8);
 	this->setPoints(10);
-	this->setHealth(2);
+	this->setHealth(1);
+	this->xIndex = 0;
+	
+	this->xMovePattern = new short[this->patternLength];
+	this->xMovePattern[0] = -16;
+	this->xMovePattern[1] = -16;
+	this->xMovePattern[2] = -16;
+	this->xMovePattern[3] = -16;
+	this->xMovePattern[4] = 16;
+	this->xMovePattern[5] = 16;
+	this->xMovePattern[6] = 16;
+	this->xMovePattern[7] = 16;
+	this->xMovePattern[8] = 16;
+	this->xMovePattern[9] = 16;
+	this->xMovePattern[10] = 16;
+	this->xMovePattern[11] = 16;
+	this->xMovePattern[12] = -16;
+	this->xMovePattern[13] = -16;
+	this->xMovePattern[14] = -16;
+	this->xMovePattern[15] = -16;
 }
 
 void Dog::move()
 {
 	if (this->getTimer() == 10 - this->getSpeed())
-	{
-		this->setYPos(this->getYPos() + this->getSpeed());
+	{				
+		this->setYPos(this->getYPos() + this->getSpeed());		
+		this->setXPos(this->getXPos() + this->xMovePattern[this->xIndex]);
 		this->setTimer(0);
+		this->xIndex++;
+		if (this->xIndex == this->patternLength)
+			this->xIndex = 0;
 	}
 	else
 		this->setTimer(this->getTimer() + 1);
--- GetOffMyLawn/Dog.h
+++ GetOffMyLawn/Dog.h
@@ -6,4 +6,8 @@ class Dog :	public GenericEnemy
 public:
 	Dog(short x, short y);
 	void move();
+private:	
+	const unsigned short patternLength = 16;
+	short* xMovePattern;
+	unsigned short xIndex;
 };
